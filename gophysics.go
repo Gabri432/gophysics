83ac2c642033bbcf2693a7347724990d45707328
Gabri432 <gabrielegatti432@gmail.com>
2022-08-30 20:02:48 +0200

    Creating functions for most basic Physics calculations.

--- gophysics.go
+++ gophysics.go
@@ -1,18 +1,59 @@
 // A package that provides different functions for making some of the most common calculation in Physics.
 //
-// Also if you find any issue or any idea to improve the program you can use this link https://github.com/Gabri432/gophysics/issues/new.
+// As so this package does not claim to be the most extensive or the best one, but it is thought as a useful tool for the most common cases.
 //
-// If you enjoyed using this program also consider putting a star in the github repository at  https://github.com/Gabri432/gophysics.
+// If you find any issue or any idea to improve the program you can use this link https://github.com/Gabri432/gophysics/issues/new.
+//
+// Also If you enjoyed using this program consider putting a star in the github repository at  https://github.com/Gabri432/gophysics.
+//
+// The package is under the MIT License https://github.com/Gabri432/gophysics/blob/master/license
 //
 // Thank you!! :)
 package gophysics
 
 import "math"
 
+//Power calls the math.Pow method to return base**power
 func Power(base, power float64) float64 {
 	return math.Pow(base, power)
 }
 
+//Power10 calls the math.Pow10 method to return base*10**power
 func Power10(base float64, power int) float64 {
 	return base * math.Pow10(power)
+}
+
+//Force is the product between mass and acceleration
+func Force(mass, acceleration float64) float64 {
+	return mass * acceleration
+}
+
+//Speed is the distance/time ratio
+func Speed(distance, time float64) float64 {
+	return distance / time
+}
+
+//Time is the distance/speed ratio
+func Time(distance, speed float64) float64 {
+	return distance / speed
+}
+
+//Work is the product between force and distance
+func Work(force, distance float64) float64 {
+	return force * distance
+}
+
+//Acceleration is the force/mass ratio
+func Acceleration(force, mass float64) float64 {
+	return force / mass
+}
+
+//Density is the weight/volume ratio
+func Density(weight, volume float64) float64 {
+	return weight / volume
+}
+
+//Intensity is the power/area ratio
+func Intensity(power, area float64) float64 {
+	return power / area
 }
