33c11bc0bc6c0291bb71cbfbb46d663b7a99b585
Gabri432 <gabrielegatti432@gmail.com>
2022-08-31 15:37:17 +0200

    Adding reference book from where formulas were taken.

--- gophysics.go
+++ gophysics.go
@@ -4,6 +4,8 @@
 //
 // As so this package does not claim to be the most extensive or the best one, but it is thought as a useful tool for the most common cases.
 //
+// Formulas were taken from this italian book: Title - "Fisica Volume 1", Authors - ["P. Mazzoldi", "M. Nigro", "C. Voci"].
+//
 // If you find any issue or any idea to improve the program you can use this link https://github.com/Gabri432/gophysics/issues/new.
 //
 // Also If you enjoyed using this program consider putting a star in the github repository at  https://github.com/Gabri432/gophysics.
