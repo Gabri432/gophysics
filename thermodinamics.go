cb4612f9d14968fa8a7933a103a5dd7362943c71
Gabri432 <gabrielegatti432@gmail.com>
2022-09-02 14:45:47 +0200

    Defining custom type 'GasBody' for handling thermodinamics related functions.

--- thermodinamics.go
+++ thermodinamics.go
@@ -1,26 +1,41 @@
 package gophysics
 
-//Gay-Lussac first law  (Volume variation), check for more information https://en.wikipedia.org/wiki/Gay-Lussac%27s_law
+// Defining custom Gas type with its own volume, pressure and temperature in celsius degrees.
+type GasBody struct {
+	Volume, Pressure, TemperatureCelsius float64
+}
+
+// Gay-Lussac first law  (Volume variation), check for more information https://en.wikipedia.org/wiki/Gay-Lussac%27s_law
+func (g GasBody) LawGayLussacVolume() float64 {
+	return LawGayLussacVolume(g.Volume, g.TemperatureCelsius)
+}
+
+// Gay-Lussac second law  (Volume variation), check for more information https://en.wikipedia.org/wiki/Gay-Lussac%27s_law
+func (g GasBody) LawGayLussacPressure() float64 {
+	return LawGayLussacPressure(g.Pressure, g.TemperatureCelsius)
+}
+
+// Gay-Lussac first law  (Volume variation), check for more information https://en.wikipedia.org/wiki/Gay-Lussac%27s_law
 func LawGayLussacVolume(volume, celsiusDeg_Temperature float64) float64 {
 	return volume * (1 + (1/273)*(celsiusDeg_Temperature))
 }
 
-//Gay-Lussac second law (Pressure Variation), check for more information https://en.wikipedia.org/wiki/Gay-Lussac%27s_law
+// Gay-Lussac second law (Pressure Variation), check for more information https://en.wikipedia.org/wiki/Gay-Lussac%27s_law
 func LawGayLussacPressure(pressure, celsiusDeg_Temperature float64) float64 {
 	return pressure * (1 + (1/273)*(celsiusDeg_Temperature))
 }
 
-//Net Heat Energy Transfer, check for more information https://en.wikipedia.org/wiki/Rate_of_heat_flow
+// Net Heat Energy Transfer, check for more information https://en.wikipedia.org/wiki/Rate_of_heat_flow
 func NetHeatEnergyTransfer(thermalConductivityConstant, area, kelvinDeg_HeatVariation, time, width float64) float64 {
 	return -1 * (thermalConductivityConstant * area * kelvinDeg_HeatVariation * time) / (width)
 }
 
-//Heat Flux, check for more information https://en.wikipedia.org/wiki/Thermal_conductivity
+// Heat Flux, check for more information https://en.wikipedia.org/wiki/Thermal_conductivity
 func HeatFlux(thermalConductivityConstant, startingTemperatureKelvin, finalTemperatureKelvin, distance float64) float64 {
 	return -1 * thermalConductivityConstant * (finalTemperatureKelvin - startingTemperatureKelvin) / distance
 }
 
-//Joule Heating, check for more information https://en.wikipedia.org/wiki/Joule_heating
+// Joule Heating, check for more information https://en.wikipedia.org/wiki/Joule_heating
 func JouleHeating(resistance, currentIntensity float64) (power float64) {
 	return resistance * (currentIntensity * currentIntensity)
 }
