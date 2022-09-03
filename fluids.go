dba3837e3887b42d9e0ec7d368809acba215789c
Gabri432 <gabrielegatti432@gmail.com>
2022-09-03 16:09:06 +0200

    Fluids functions will return a value and a measurement unit as outputs.

--- fluids.go
+++ fluids.go
@@ -3,11 +3,11 @@ package gophysics
 import "math"
 
 //Hagen-Poiseuille law, check for more information https://en.wikipedia.org/wiki/Hagen%E2%80%93Poiseuille_equation
-func LawHagenPoiseuille(fluidViscosity, pipeLength, flowRate, pipeRadius float64) (pressureDifference float64) {
-	return ((8 * fluidViscosity * pipeLength * flowRate) / (3.14159 * math.Pow(pipeRadius, 4)))
+func LawHagenPoiseuille(fluidViscosity, pipeLength, flowRate, pipeRadius float64) (pressureDifference float64, measurementUnit string) {
+	return ((8 * fluidViscosity * pipeLength * flowRate) / (3.14159 * math.Pow(pipeRadius, 4))), "Pascal"
 }
 
 //Stokes law, check for more information https://en.wikipedia.org/wiki/Stokes%27_law
-func LawStokes(fluidViscosity, radius, speed float64) (force float64) {
-	return 6 * 3.14159 * fluidViscosity * radius * speed
+func LawStokes(fluidViscosity, radius, speed float64) (force float64, measurementUnit string) {
+	return 6 * 3.14159 * fluidViscosity * radius * speed, "N"
 }
