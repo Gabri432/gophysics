061c1104237704f3e383aecae943b8642e11485f
Gabri432 <gabrielegatti432@gmail.com>
2022-09-04 10:16:08 +0200

    Creating the package 'math' to allow some mathematical conversion for physics.

---
+++ math/constants.go
@@ -0,0 +1,18 @@
+package math
+
+const (
+	//Greek Pi
+	Pi, GreekPi = 3.1415926535, Pi
+
+	//1 Radiant in degrees
+	Radiant = 57.2958
+
+	//Euler number
+	E = 2.71828
+
+	//Square root of 2
+	SquareRoot_2 = 1.41421
+
+	//Square root of 3
+	SquareRoot_3 = 1.73205
+)
